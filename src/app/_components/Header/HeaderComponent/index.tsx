7a12da1af6045e965df7fa3ae93a7d19cf352c55
nathan <000savior001@gmail.com>
2024-03-27 22:31:56 -0700

    noHeaderFooterUrls list added | to hide headerComponent to specified path

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants';
 import { Gutter } from '../../Gutter'
 import MobileNav from '../MobileNav';
 import { HeaderNav } from '../Nav';
@@ -9,8 +12,14 @@ import { HeaderNav } from '../Nav';
 import classes from './index.module.scss';
 
 const HeaderComponent = ({ header }: { header: Header}) => {
+  const pathname = usePathname()
   return (
-    <nav className={[classes.header]}>
+    <nav className={[
+      classes.header, noHeaderFooterUrls.includes(pathname) 
+      && classes.hide]
+      .filter(Boolean)
+      .join(" ")
+                    }>
         <Gutter>
             <Link href="/">
                 <Image src="/logo-black.svg" alt="logo" width={170} height={50}/>
