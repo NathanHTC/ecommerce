f00a714567c91f5e3874be22d0db833fe7835b67
nathan <000savior001@gmail.com>
2024-03-28 22:23:59 -0700

    added footer hidden url

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,9 +1,22 @@
+'use client'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import { noHeaderFooterUrls } from '../../../constants'
+import { Gutter } from '../../Gutter'
 import classes from './index.module.scss'
 
 const FooterComponent = () => {
+  const pathname = usePathname();
   return (
-    <div>FooterComponent</div>
+    <footer className={
+      noHeaderFooterUrls.includes(pathname) ? classes.hide : " "}
+    >
+      <Gutter>
+        <ul className={classes.inclusions}>
+
+        </ul>
+      </Gutter>
+    </footer>
   )
 }
 
