ab6e5d25e5e8dacb038f406a2f482c52a4407d20
nathan <000savior001@gmail.com>
2024-04-01 22:42:11 -0700

    recover password page modified

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -6,12 +6,42 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.RecoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image 
+            src="logo-black.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          /> 
+        </Link>
+      </div>
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Forgot Password</h3>
+            <Image 
+              src="/assets/icons/hand.png"
+              alt="hand"
+              width={30}
+              height={30}
+            />
+          </div>
+
+          <RecoverPasswordForm />
+        </div>
+      </div>
+
+    </section>
   )
 }
 
