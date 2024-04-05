3ceecc792a1dbbc12838f9ee9efcef3c1a6bef89
nathan <000savior001@gmail.com>
2024-04-04 22:47:57 -0700

    categories query modified

--- src/app/_graphql/categories.ts
+++ src/app/_graphql/categories.ts
@@ -1,4 +1,4 @@
-export const CATEGORIES = `
+export const PRODUCT_CATEGORIES = `#graphql
 categories {
   title
   id
@@ -7,3 +7,20 @@ categories {
     label
   }
 }`
+
+export const CATEGORIES = `#graphql
+  query Categories {
+    Categories(limit:300){
+      docs {
+        id
+        title
+        media {
+          alt
+          width
+          height
+          url
+        }
+      }
+    }
+  }
+`
