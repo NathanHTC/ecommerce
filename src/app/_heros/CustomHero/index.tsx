35785fd6e94c0be3f2d629bf33ac5c087dc3ce6c
nathan <000savior001@gmail.com>
2024-04-04 19:19:22 -0700

    media from payload is used as background img | customHero section

--- src/app/_heros/CustomHero/index.tsx
+++ src/app/_heros/CustomHero/index.tsx
@@ -9,11 +9,13 @@ import RichText from '../../_components/RichText'
 import classes from './index.module.scss'
 
 export const CustomHero: React.FC<Page['hero']> = ({ richText, media, links }) => {
+  const mediaUrl = media && typeof media !== 'string' && `${process.
+  env.NEXT_PUBLIC_SERVER_URL}/media/${media.filename}`
 
-  
   return (
     <section className={classes.hero}>
-      <div className={classes.heroWrapper}>
+      <div className={classes.heroWrapper} style={{ backgroundImage:
+      `url(${mediaUrl})` }}>
         <RichText content={richText} />
         {Array.isArray(links) && links.length > 0 && (
           <ul className={classes.links}>
@@ -25,19 +27,6 @@ export const CustomHero: React.FC<Page['hero']> = ({ richText, media, links }) =
               )
             })}
           </ul>
-        )}
-      </div>
-      <div className={classes.media}>
-        {typeof media === 'object' && (
-          <Fragment>
-            <Media
-              resource={media}
-              // fill
-              imgClassName={classes.image}
-              priority
-            />
-            {media?.caption && <RichText content={media.caption} className={classes.caption} />}
-          </Fragment>
         )}
       </div>
     </section>
