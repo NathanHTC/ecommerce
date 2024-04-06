ef18fdf7c6fe88165dc924f796c178a4aeae0634
nathan <000savior001@gmail.com>
2024-04-06 00:48:12 -0700

    media bug fixed | categories component working

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,18 +1,28 @@
-import Link from 'next/link'
+'use client'
 import React from 'react'
-import { Category } from '../../../../payload/payload-types'
+import Link from 'next/link'
+
+import { Category, Media } from '../../../../payload/payload-types'
+// import { useFilter } from '../../../_providers/Filter'
+
 import classes from './index.module.scss'
 
 type CategoryCardProps = {
-    category: Category
+  category: Category
 }
+
 const CategoryCard = ({ category }: CategoryCardProps) => {
+  const media = category.media as Media
+//   const { setCategoryFilters } = useFilter()
+
   return (
     <Link
-        href="/products"
-        className={classes.card}
+      href="/products"
+      className={classes.card}
+      style={{ backgroundImage: `url(${media.url})` }}
+    //   onClick={() => setCategoryFilters([category.id])}
     >
-        <p className={classes.title}>{category.title}</p>
+      <p className={classes.title}>{category.title}</p>
     </Link>
   )
 }
