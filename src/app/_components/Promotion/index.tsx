b0b69b3997aecbf3d94dcff2134a5c610a7fcf4e
nathan <000savior001@gmail.com>
2024-04-22 14:02:24 -0700

    statBox and timer state added | Promotion component

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import classes from './index.module.scss'
 
 const Promotion = () => {
-  return (
-    <div>Promotion</div>
-  )
+    //create a timer state for StatBox
+    //use useEffect to update timer every sec without rerendering
+    const [time, setTime] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    })
+    return (
+        <section className={classes.promotion}>
+            <div className={classes.textBox}>
+                <h3 className={classes.title}>Deals of the Month</h3>
+                <p>
+                    Get ready for a shopping experience like never before with our Deals of the Month! Every
+                    purchase comes with exclusive perks and offers, making this month a celebration of savvy
+                    choices and amazing deals. Don't miss out! 🎁🛒
+                </p>
+
+                <ul className={classes.stats}>
+                    <StatBox label="Days" value={time.days} />
+                    <StatBox label="Hours" value={time.hours} />
+                    <StatBox label="Minutes" value={time.minutes} />
+                    <StatBox label="Seconds" value={time.seconds} />
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+
+const StatBox = ({ label, value }: { label: string; value: number }) => {
+    return (
+        <li className={classes.statBox}>
+            <h4>{value}</h4>
+            <p>{label}</p>
+        </li>
+    )
 }
 
 export default Promotion
