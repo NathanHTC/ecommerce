c1359370bca755ae3ee374272913797b5ea22beb
nathan <000savior001@gmail.com>
2024-04-22 12:53:44 -0700

    Promotion component and style added

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -66,6 +66,7 @@ export default async function Page({ params: { slug = 'home' } }) {
           
           <Gutter className={classes.home}>
             <Categories categories={categories}/>
+            <Promotion />
           </Gutter>
         </section>
 
